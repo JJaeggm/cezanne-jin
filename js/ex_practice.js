f6f3bcaa3e740d6e6c1270b7f7a9a515ea426443
JJaeggm <raptor1130@gmail.com>
2023-11-29 02:32:17 +0900

    menu movement 1

--- js/ex_practice.js
+++ js/ex_practice.js
@@ -1,5 +1,18 @@
 'use strict';
 
-function bookmarkButton() {
-    if(${})
-}
+// 1. element를 조작할 때는 DOM이 로드가 되어 있어야 한다.
+// 2. class는 snake, id는 camelCase
+// 3. classList. 여러가지 메소드 있음 (class 조작)
+// 3. getElementById는 1개 반환, getElementsByClassName은 여러개 반환
+// 4. .querySelector('#menu_button > label')
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const menuFrame = document.getElementsByClassName("menu_frame")[0] // class => []
+    console.log("menuFrame", menuFrame) // undefined
+        
+    menuFrame.addEventListener("click", () => {
+        menuFrame.classList.toggle("open")
+        console.log(menuFrame.classList)
+    })
+   });
